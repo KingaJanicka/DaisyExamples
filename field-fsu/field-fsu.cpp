c6e470a2f5aff67d554cb32b5af9f8291dc366ff
Kinga Janicka <kingajanicka7@gmail.com>
2023-09-08 14:51:50 +0100

    Deleted the NlFIlt, added the filter library, got the wavefolder to a decent point

--- field-fsu/field-fsu.cpp
+++ field-fsu/field-fsu.cpp
@@ -8,34 +8,41 @@ using namespace daisysp;
 
 DaisyField hw;
 Svf         svf;
-NlFilt      nfilt;
 Fold        fold;
-Wavefolder  wavefolder;
-Overdrive   overdrive;
+Wavefolder  wavefolder1;
+Wavefolder  wavefolder2;
+Overdrive   overdrive1;
+Overdrive   overdrive2;
+Limiter     limiter;
 DcBlock     dcblock;
 
-bool  effectOn;
+bool  effectOn = 1;
 float wet;
 
 float svfCutoff;
 float svfLP;
 float svfHP;
 
-float nlFiltOut;
+float wavefolder1Gain;
+float wavefolder1Offset;
 
-float wavefolderGain;
-float wavefolderOffset;
+float wavefolder2Gain;
+float wavefolder2Offset;
+float overdrive2Out;
+float overdrive2Drive;
+float wavefolder2BlockWet;
 
-float overdriveOut;
-float overdriveDrive;
 
 float filFreq;
 float filRes;
 
+float limiterPreGain;
 
 float effectVolume;
 float lastSample;
 float finalOut;
+size_t blockSize = 4;
+
 char cstr[15];
 char displayBuffer[15]; 
 
@@ -64,17 +71,6 @@ void Controls()
 {
     // This bit processes knob movement and translates it into float/init values
     hw.ProcessAllControls();
-
-    /// Add pages to the knobs;
-  switch (page){
-    case 0:
-    svfCutoff = hw.knob[0].Process();
-    break;
-
-    case 1:
-    wavefolderGain = hw.knob[0].Process();
-    break;
-  }
     effectOn ^= hw.sw[0].RisingEdge();
 }
 
@@ -126,7 +122,7 @@ void drawControl(char * buffer, const char* paramName, int paramValue, int posit
     hw.display.WriteString(displayBuffer, Font_7x10, true);
 
 }
-void drawText(char * buffer, const char* paramName){
+void drawMenuLabel(char * buffer, const char* paramName){
     hw.display.SetCursor(30, 50);
     sprintf(displayBuffer, "%s", paramName);
     hw.display.WriteString(displayBuffer, Font_7x10, true);
@@ -144,16 +140,16 @@ void AudioCallback(AudioHandle::InputBuffer  in,
     {
         // Set Controls Here
 
-        // Soap Controls
+        // SVF Controls
         svf.SetFreq(hw.AudioSampleRate() * pow(svfCutoff, 2) * 0.15);
         
 
+
         // Wavefolder Controls        the offset has -0.5 so it can ofset to negative too
-        wavefolder.SetGain(wavefolderGain);
-        wavefolder.SetOffset(wavefolderOffset - 0.5f);
-        
+        wavefolder2.SetGain(wavefolder2Gain*15.f);
+        wavefolder2.SetOffset(wavefolder2Offset - .5f);
         // Overdrive Controls
-        overdrive.SetDrive(overdriveDrive);
+        overdrive2.SetDrive(overdrive2Drive);
 
         out[0][i] = in[0][i];
         out[1][i] = in[0][i];
@@ -161,7 +157,6 @@ void AudioCallback(AudioHandle::InputBuffer  in,
         // Actual FX code below
         if(effectOn)
         {
-            // SVF for split into Nfilt 
 
 			// pha = phaser.Process(in[0][i]) * wet + in[0][i] * (1.f - wet);
            
@@ -169,13 +164,12 @@ void AudioCallback(AudioHandle::InputBuffer  in,
             svfLP = svf.Low();
             svfHP = svf.High();
 
-            nfilt.ProcessBlock(&svfHP, &nlFiltOut, hw.AudioSampleRate());
-
-			overdriveOut = overdrive.Process(wavefolder.Process(svf.High()));
+			overdrive2Out = overdrive2.Process(wavefolder2.Process(svfHP));
         
 
+
             // Final output
-            out[0][i] = out[1][i] = overdriveOut;
+            out[0][i] = out[1][i] = overdrive2Out * wavefolder2BlockWet + svfHP * (1.0f - wavefolder2BlockWet) + svfLP;
 
         }
     }
@@ -186,6 +180,7 @@ int main(void)
     
     hw.Init();
     float sample_rate = hw.AudioSampleRate();
+    hw.SetAudioBlockSize(blockSize);
     page = 0;
 
     // SVF Init
@@ -193,20 +188,26 @@ int main(void)
     filFreq = 0.2f;
     filRes = 0.0f;
 
-    //NlFilt
-    nfilt.Init();
+    // Wavefolder1 init
+    wavefolder1.Init();
+    wavefolder1Gain = 0.5f;
+    wavefolder1Offset = 0.5f;
 
     // Fold init
     fold.Init();
 
-    // Wavefolder init
-    wavefolder.Init();
-    wavefolderGain = 0.5f;
-    wavefolderOffset = 0.5f;
+    // Wavefolder2 init
+    wavefolder2.Init();
+    wavefolder2Gain = 0.1f;
+    wavefolder2Offset = 0.1f;
+    wavefolder2BlockWet = 1.0f;
 
     // Overdrive init
-    overdrive.Init();
-    overdriveDrive = 0.1f;
+    overdrive2.Init();
+    overdrive2Drive = 0.1f;
+
+    // Limiter init
+    limiter.Init();
 
     // DC block init
     dcblock.Init(sample_rate);
@@ -222,16 +223,51 @@ int main(void)
         // This bit is responsible for drawing on screen
         
         hw.display.Fill(false);
-        // example of the new function working
-        //Handle Menu Pages draw
+       
+        hw.display.SetCursor(00, 50);
+        sprintf(displayBuffer, "%s", effectOn ? "On" : "Off", effectOn);
+        hw.display.WriteString(displayBuffer, Font_7x10, true);
+
+        //Handle Menu Pages draw and knob assignment
        switch (page){
         case 0 :
-        drawControl(displayBuffer, "FREQ ",(int)(svfCutoff * 100), 1);
-        drawText(displayBuffer, "FILTER");
+        drawControl(displayBuffer, "SPLIT F",(int)(svfCutoff * 100), 3);
+        svfCutoff = hw.knob[0].Process();
+        drawMenuLabel(displayBuffer, "MIXER");
         break;
-        case 1:
-        drawControl(displayBuffer, "FOLD ",(int)(wavefolderGain * 100), 1);
-        drawText(displayBuffer, "WAVEFOLDER");
+
+        case 1 :
+        drawMenuLabel(displayBuffer, "WAVEFOLDER 1");
+        break;
+        
+        case 2 :
+   
+
+        drawMenuLabel(displayBuffer, "FILTER");
+        break;
+        
+        case 3:
+        drawControl(displayBuffer, "FOLD ",(int)(wavefolder2Gain * 100), 1);
+        wavefolder2Gain = hw.knob[0].Process();
+
+        drawControl(displayBuffer, "OFFSET ",(int)(wavefolder2Offset * 100), 2);
+        wavefolder2Offset = hw.knob[1].Process();
+        
+        drawControl(displayBuffer, "DRIVE ",(int)(overdrive2Drive * 100), 3);
+        overdrive2Drive = hw.knob[2].Process();
+
+        drawControl(displayBuffer, "WET ",(int)(wavefolder2BlockWet * 100), 4);
+        wavefolder2BlockWet = hw.knob[3].Process();
+        
+        drawMenuLabel(displayBuffer, "WAVEFOLDER 2");
+        break;
+
+        case 4:
+        drawMenuLabel(displayBuffer, "DECIMATE");
+        break;
+
+        case 5:
+        drawMenuLabel(displayBuffer, " ");
         break;
        } 
 
